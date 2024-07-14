c16313603eb0f6a175eead6c408d0b9660fe785a
ridazi <158287125+ridazi@users.noreply.github.com>
2024-07-14 15:32:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// const minNum =1;
+// const maxNum = 50;
+// const answer = Math.floor(Math.random()*(maxNum-minNum +1))+ minNum;
+
+// let attempts = 0;
+// let guess;
+// let running = true;
+//  while(running){
+//     guess = window.prompt(`guess a number ${minNum}- ${maxNum}`)
+//     guess = Number(guess);
+//     if(isNaN(guess)){
+//         window.alert("please enter a valid number");
+//     }
+//     else if(guess< minNum || guess> maxNum){
+//         window.alert("please enter a valid number");
+//     }
+//     else{
+// attempts++;
+// if(guess < answer){
+// window.alert("too low! try Again");
+
+// }
+// else if(guess > answer){
+//     window.alert("too high! try Again");
+// }
+// else{
+//     window.alert(`Correct! the answer was ${answer}. it took you ${attempts}`)
+//     running = false;
+// }
+//     }
+  
+    
+//  }
